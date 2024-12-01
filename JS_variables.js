a42339c2f30407dc07ed2815c92f555d4970f023
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2024-12-01 22:52:36 +0530

    my first commit

---
+++ JS_variables.js
@@ -0,0 +1,21 @@
+let message = "hello world";
+
+console.log(message);
+
+const myBirthday = "16-12-1999";
+console.log(myBirthday);
+
+//myBirthday = "17-12-1999"; cannot assign to a constant variable, assignment can only be done once.
+
+//UpperCase Constants
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+console.log(color); // #FF7F00
+//Being a “constant” just means that a variable’s value never changes. But some constants are known before execution (like a hexadecimal value for red) and some constants are calculated in run-time, during the execution, but do not change after their initial assignment.
+
